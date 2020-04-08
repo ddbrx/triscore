57e4f91cdfc7514a12cb1c35670a85e12a537dd8
Dima Dobrynin <sh1m423@gmail.com>
2020-04-08 22:19:37 +0000

    Add mongosync script

--- scripts/mongosync.sh
+++ scripts/mongosync.sh
@@ -1,5 +1,8 @@
-# Dump source database
+# Dump database on source server
 mongodump --db triscore --out triscore-backup-2020-04-06
 
+# Copy to target server
+rsync -avR triscore-backup-2020-04-06 static.135.98.202.116.clients.your-server.de:/home/dmitriyd/
+
 # Restore database from dump
-sudo mongorestore --db triscore --drop triscore-backup-2020-04-06/triscore
+mongorestore --db triscore --drop triscore-backup-2020-04-06/triscore --numParallelCollections 1

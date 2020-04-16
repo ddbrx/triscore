32e7de53ddfbc9d272f29ec1f748ee4afa563fc7
Dima Dobrynin <sh1m423@gmail.com>
2020-04-16 12:49:34 +0300

    Fix matcher no country

--- race/matcher.py
+++ race/matcher.py
@@ -39,73 +39,53 @@ def get_min_max_birth_dates(age_group, date):
     return min_birth_date, max_birth_date
 
 
-def process_race(race_name, race_date, race_results, name_map):
+def process_race(race_name, race_date, race_country, race_results, name_map):
     for race_result in race_results:
-        age_group = race_parser.get_age_group(race_result)
-        # if age_group.find('-') == -1:
-        #     continue
-
         athlete_name = race_parser.get_athlete_name(race_result)
-        # encoded_athlete_name = athlete_name.encode('utf-8')
 
         athlete_id = race_parser.get_athlete_id(race_result)
         athlete_country = race_parser.get_country_fifa_code(race_result)
 
-        nc_key = f'{athlete_name}:{athlete_country}'
+        if athlete_name not in name_map:
+            name_map[athlete_name] = {}
 
-        if nc_key not in name_map:
-            name_map[nc_key] = {}
-
-        if athlete_id not in name_map[nc_key]:
-            name_map[nc_key][athlete_id] = {
+        if athlete_id not in name_map[athlete_name]:
+            name_map[athlete_name][athlete_id] = {
                 'dmin': dt.datetime(1930, 1, 1), 'dmax': dt.delta(dt.now(), years=18), 'races': []}
 
+        age_group = race_parser.get_age_group(race_result)
         min_birth_date, max_birth_date = get_min_max_birth_dates(
             age_group, dt.date_from_string(race_date))
 
-        name_map[nc_key][athlete_id]['dmin'] = max(
-            name_map[nc_key][athlete_id]['dmin'], min_birth_date)
-        name_map[nc_key][athlete_id]['dmax'] = min(
-            name_map[nc_key][athlete_id]['dmax'], max_birth_date)
-        name_map[nc_key][athlete_id]['races'].append(
-            {'d': race_date, 'n': race_name, 'a': age_group})
+        name_map[athlete_name][athlete_id]['dmin'] = max(
+            name_map[athlete_name][athlete_id]['dmin'], min_birth_date)
+        name_map[athlete_name][athlete_id]['dmax'] = min(
+            name_map[athlete_name][athlete_id]['dmax'], max_birth_date)
+        name_map[athlete_name][athlete_id]['races'].append(
+            {'rd': race_date, 'rn': race_name, 'rc': race_country, 'a': age_group, 'c': athlete_country})
 
 
 def insert_to_mongo(name_map, db_name, collection_name):
     mongo_client = MongoClient()
     collection = mongo_client[db_name][collection_name]
     collection.create_index('n')
-    collection.create_index('c')
-    collection.create_index('l.a')
     collection.create_index('l.id')
 
-    for nc_key, id_map in name_map.items():
-        # if len(id_map) == 1:
-        #     continue
+    for athlete_name, id_map in name_map.items():
         nc_athletes = []
         for athlete_id, dinfo in id_map.items():
             races = dinfo['races']
-            age_group_count = {}
-            for race in races:
-                age_group = race['a']
-                if age_group not in age_group_count:
-                    age_group_count[age_group] = 0
-                age_group_count[age_group] += 1
-            age_group = sorted(age_group_count.items(), key=lambda item: -item[1])[0][1]
 
             athlete_info = {
                 'id': athlete_id,
-                'a': age_group,
                 'dmin': dt.date_to_string(dinfo['dmin']),
                 'dmax': dt.date_to_string(dinfo['dmax']),
                 'races': races
             }
             nc_athletes.append(athlete_info)
 
-        name, country = nc_key.split(':')
         nc_info = {
-            'n': name,
-            'c': country,
+            'n': athlete_name,
             'l': nc_athletes
         }
 
@@ -129,12 +109,16 @@ def main():
     for i, race_info in enumerate(races):
         race_name = race_parser.get_race_name(race_info)
         race_date = race_parser.get_race_date(race_info)
+        race_country = race_parser.get_race_fifa_code(race_info)
+
         race_results = race_storage.get_race_results(race_name, race_date)
 
-        logger.info(f'{i + 1}/{count}: {race_date} {race_name}')
-        process_race(race_name, race_date, race_results, name_map)
+        logger.info(f'{i + 1}/{count}: {race_date} {race_name} {race_country}')
+        process_race(race_name, race_date, race_country,
+                     race_results, name_map)
 
-    insert_to_mongo(name_map, db_name='triscore-test', collection_name='matcher')
+    insert_to_mongo(name_map, db_name='triscore-test',
+                    collection_name='matcher')
 
 
 if __name__ == '__main__':
